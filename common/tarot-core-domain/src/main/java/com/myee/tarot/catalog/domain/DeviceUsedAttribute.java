084a55d96c80fb2ac2ca9746b3de9ca60f5a0ed8
envacom <innovateevery@163.com>
2016-05-27 11:36:08 +0800

    测试

--- common/tarot-core-domain/src/main/java/com/myee/tarot/catalog/domain/DeviceUsedAttribute.java
+++ common/tarot-core-domain/src/main/java/com/myee/tarot/catalog/domain/DeviceUsedAttribute.java
@@ -23,6 +23,7 @@ public class DeviceUsedAttribute extends GenericEntity<Long, DeviceUsedAttribute
     @Column(name = "VALUE")
     protected String value;
 
+
     @Column(name = "SEARCHABLE")
     protected boolean searchable = false;
 
