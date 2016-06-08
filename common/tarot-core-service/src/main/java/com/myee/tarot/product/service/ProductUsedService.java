477460965b46b98846fec71b45be3070ce847384
envacom <innovateevery@163.com>
2016-06-08 11:46:33 +0800

    产品

---
+++ common/tarot-core-service/src/main/java/com/myee/tarot/product/service/ProductUsedService.java
@@ -0,0 +1,11 @@
+package com.myee.tarot.product.service;
+
+import com.myee.tarot.catalog.domain.ProductUsed;
+import com.myee.tarot.core.service.GenericEntityService;
+
+/**
+ * Created by Enva on 2016/6/1.
+ */
+public interface ProductUsedService extends GenericEntityService<Long, ProductUsed> {
+
+}
