c7d177fd6c30835d5b687080aa4b6b1d2a359b04
martin <martin.xu@clever-m.com>
2016-07-20 15:07:54 +0800

    前端查询过滤参数。

---
+++ website/tarot-home/src/main/java/com/myee/tarot/web/WhereRequest.java
@@ -0,0 +1,16 @@
+package com.myee.tarot.web;
+
+/**
+ * Created by Martin on 2016/7/20.
+ */
+public class WhereRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
