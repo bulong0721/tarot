c55a029e5f1eb5c6ac3174aac330d708f660dc8c
Ray.Fu <ray.fu@clever-m.com>
2016-07-26 09:48:21 +0800

    修改实体的字段定义

--- common/tarot-core-domain/src/main/java/com/myee/tarot/datacenter/domain/SelfCheckLogVO.java
+++ common/tarot-core-domain/src/main/java/com/myee/tarot/datacenter/domain/SelfCheckLogVO.java
@@ -4,63 +4,63 @@ package com.myee.tarot.datacenter.domain;
  * Created by Ray.Fu on 2016/7/15.
  */
 public class SelfCheckLogVO {
-    private Long mTime;
+    private Long time;
 
-    private Integer mEventLevel;
+    private Integer eventLevel;
 
-    private Integer mModuleId;
+    private Integer moduleId;
 
-    private Integer mFunctionId;
+    private Integer functionId;
 
-    private String mData;
+    private String data;
 
-    private Integer mLength;
+    private Integer length;
 
-    public Long getmTime() {
-        return mTime;
+    public Long getTime() {
+        return time;
     }
 
-    public void setmTime(Long mTime) {
-        this.mTime = mTime;
+    public void setTime(Long time) {
+        this.time = time;
     }
 
-    public Integer getmEventLevel() {
-        return mEventLevel;
+    public Integer getEventLevel() {
+        return eventLevel;
     }
 
-    public void setmEventLevel(Integer mEventLevel) {
-        this.mEventLevel = mEventLevel;
+    public void setEventLevel(Integer eventLevel) {
+        this.eventLevel = eventLevel;
     }
 
-    public Integer getmModuleId() {
-        return mModuleId;
+    public Integer getModuleId() {
+        return moduleId;
     }
 
-    public void setmModuleId(Integer mModuleId) {
-        this.mModuleId = mModuleId;
+    public void setModuleId(Integer moduleId) {
+        this.moduleId = moduleId;
     }
 
-    public Integer getmFunctionId() {
-        return mFunctionId;
+    public Integer getFunctionId() {
+        return functionId;
     }
 
-    public void setmFunctionId(Integer mFunctionId) {
-        this.mFunctionId = mFunctionId;
+    public void setFunctionId(Integer functionId) {
+        this.functionId = functionId;
     }
 
-    public String getmData() {
-        return mData;
+    public String getData() {
+        return data;
     }
 
-    public void setmData(String mData) {
-        this.mData = mData;
+    public void setData(String data) {
+        this.data = data;
     }
 
-    public Integer getmLength() {
-        return mLength;
+    public Integer getLength() {
+        return length;
     }
 
-    public void setmLength(Integer mLength) {
-        this.mLength = mLength;
+    public void setLength(Integer length) {
+        this.length = length;
     }
 }
