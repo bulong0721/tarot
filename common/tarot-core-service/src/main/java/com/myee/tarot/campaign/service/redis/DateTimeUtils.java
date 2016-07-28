9ba62107aee2ae6d5948c2cdf70bf9185184b3fb
selim.jiang <selim.jiang@clever-m.com>
2016-07-28 09:14:19 +0800

    实体类

---
+++ common/tarot-core-service/src/main/java/com/myee/tarot/campaign/service/redis/DateTimeUtils.java
@@ -0,0 +1,251 @@
+package com.myee.tarot.campaign.service.redis;
+
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.FastDateFormat;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * INFO: 日期时间工具类
+ * User: zhaokai@mail.qianwang365.com
+ * Date: 2014/9/17
+ * Time: 13:13
+ * Version: 1.0
+ * History: <p>如果有修改过程，请记录</P>
+ */
+public class DateTimeUtils {
+
+    public static final String DEFAULT_DATE_FORMAT_PATTERN_SHORT = "yyyy-MM-dd";
+
+    public static final String DEFAULT_TIME_FORMAT_PATTERN_SHORT = "HH:mm:ss";
+
+    public static final String DATE_FORMAT_PATTERN_FULL = "yyyyMMddHHmmss";
+
+    public static final String DEFAULT_DATE_FORMAT_PATTERN_FULL = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String HOUR_END = " 23:59:59";
+
+    public static final String HOUR_START = " 00:00:00";
+
+    private static Map<String, FastDateFormat> dateFormatCache = new ConcurrentHashMap<String, FastDateFormat>();
+
+    /**
+     * 以yyyy-MM-dd HH:mm:ss形式返回当前时间的字符串
+     *
+     * @return
+     */
+    public static String getCurrentDateTimeString() {
+        return getCurrentDateString(DEFAULT_DATE_FORMAT_PATTERN_FULL);
+    }
+
+    /**
+     * 以yyyy-MM-dd形式返回当前日期的字符串
+     *
+     * @return
+     */
+    public static String getCurrentDateString() {
+        return getCurrentDateString(DEFAULT_DATE_FORMAT_PATTERN_SHORT);
+    }
+
+    /**
+     * 返回pattern所指定的当前时间的字符串
+     *
+     * @param pattern
+     * @return
+     */
+    public static String getCurrentDateString(String pattern) {
+        if (pattern == null || "".equals(pattern.trim())) {
+            return null;
+        }
+        FastDateFormat sdf = null;
+        if (dateFormatCache.containsKey(pattern)) {
+            sdf = dateFormatCache.get(pattern);
+        } else {
+            try {
+                sdf = FastDateFormat.getInstance(pattern);
+                dateFormatCache.put(pattern, sdf);
+            } catch (Exception e) {
+                e.printStackTrace();
+                sdf = FastDateFormat.getInstance(DEFAULT_DATE_FORMAT_PATTERN_FULL);
+            }
+        }
+        return sdf.format(new Date());
+    }
+
+    /**
+     * 返回时间date所指定的日期格式的字符串形式
+     *
+     * @param date
+     * @param pattern
+     * @return
+     */
+    public static String getDateString(Date date, String pattern) {
+        if (date == null || pattern == null || "".equals(pattern.trim())) {
+            return null;
+        }
+        FastDateFormat sdf = null;
+        if (dateFormatCache.containsKey(pattern)) {
+            sdf = dateFormatCache.get(pattern);
+        } else {
+            try {
+                sdf = FastDateFormat.getInstance(pattern);
+                dateFormatCache.put(pattern, sdf);
+            } catch (Exception e) {
+                e.printStackTrace();
+                sdf = FastDateFormat.getInstance(DEFAULT_DATE_FORMAT_PATTERN_FULL);
+            }
+        }
+        return sdf.format(date);
+    }
+
+    /**
+     * 将dateTimeString按照格式pattern转换成Date
+     *
+     * @param dateTimeString
+     * @param pattern
+     * @return
+     */
+   public static Date getDateByString(String dateTimeString, String pattern) {
+        if (dateTimeString == null || "".equals(dateTimeString.trim()) || pattern == null || "".equals(pattern.trim())) {
+            return null;
+        }
+        FastDateFormat sdf = null;
+        try {
+            if (dateFormatCache.containsKey(pattern)) {
+                sdf = dateFormatCache.get(pattern);
+                return sdf.parse(dateTimeString);
+            } else {
+                sdf = FastDateFormat.getInstance(pattern);
+                dateFormatCache.put(pattern, sdf);
+                return sdf.parse(dateTimeString);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Date date = getDateByString("2016-5-30 23:30:34","yyyy-MM-dd HH:mm:ss");
+        System.out.println(date.getTime());
+    }
+
+    /**
+     * 将dateTimeString按照默认格式yyyy-MM-dd HH:mm:ss转换成Date
+     *
+     * @param dateTimeString
+     * @return
+     */
+    public static Date getDateByString(String dateTimeString) {
+        return getDateByString(dateTimeString, DEFAULT_DATE_FORMAT_PATTERN_FULL);
+    }
+
+    /**
+     * 获取一天开始时间
+     *
+     * @param startDate 格式yyyy-MM-dd
+     * @return
+     * @author jijun
+     * @date 2014年6月30日
+     */
+    public static Date startOneDay(String startDate) {
+        return getDateByString(startDate + HOUR_START);
+    }
+
+    /**
+     * 获取一天结束时间
+     *
+     * @param endDate 格式yyyy-MM-dd
+     * @return
+     * @author jijun
+     * @date 2014年6月30日
+     */
+    public static Date endOneDay(String endDate) {
+        return getDateByString(endDate + HOUR_END);
+    }
+
+    /**
+     * 根据时间获取默认格式yyyy-MM-dd HH:mm:ss的String表达式
+     *
+     * @param date
+     * @author jijun
+     * @date 2014年7月1日
+     */
+    public static String getDefaultDateString(Date date) {
+
+        return DateFormatUtils.format(date, DEFAULT_DATE_FORMAT_PATTERN_FULL);
+    }
+
+
+    /**
+     * 计算2个时间差,date1-date2
+     *
+     * @param date1
+     * @param date2
+     * @return 时间差，单位妙
+     */
+    public static int getTimeGap(Date date1, Date date2) {
+
+        long result = (date1.getTime() - date2.getTime()) / 60000;
+        return Integer.parseInt(String.valueOf(result));
+    }
+
+    /**
+     * 创建时间
+     *
+     * @param date1 yyyy-MM-dd
+     * @param date2 HH:mm:ss
+     * @return
+     */
+    public static Date createDate(String date1, String date2) {
+
+        try {
+            return DateTimeUtils.getDateByString(date1 + " " + date2);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 获取客户端要求的日期格式yyyy.MM.dd
+     *
+     * @param dateTime
+     * @return
+     */
+    public static String convertToAppClientDateFormat(String dateTime) {
+        if (dateTime != null) {
+            return dateTime.replace("-", ".");
+        }
+        return dateTime;
+    }
+
+    /**
+     * 获取服务端要求的日期格式yyyy-MM-dd
+     *
+     * @param dateTime
+     * @return
+     */
+    public static String convertToServerDateFormat(String dateTime) {
+        if (dateTime != null) {
+            return dateTime.replace(".", "-");
+        }
+        return dateTime;
+    }
+
+    /**
+     * 获取距离现在N天后/前的时间
+     *
+     */
+    public static Date getInternalDateByDay(Date d, int days) {
+        Calendar now = Calendar.getInstance(TimeZone.getDefault());
+        now.setTime(d);
+        now.add(Calendar.DATE, days);
+        return now.getTime();
+    }
+
+}
---
+++ common/tarot-core-service/src/main/java/com/myee/tarot/campaign/service/redis/ObjectUtils.java
@@ -0,0 +1,147 @@
+package com.myee.tarot.campaign.service.redis;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Array;
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
+import java.util.Collection;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * 有关Object的工具类
+ */
+public class ObjectUtils {
+    private static Logger logger = LoggerFactory.getLogger(ObjectUtils.class);
+
+    // 半角/全角 字母 数字  及汉字
+    public static final String PRODUCT_NAME_PATTERN = "[(a-zA-Z0-9\\u4e00-\\u9fa5\\uff10-\\uff19\\uff21-\\uff3a\\uff41-\\uff5a)]{1,30}";
+    // public static final String PRODUCT_NAME_PATTERN = "[(a-zA-Z0-9\\u4e00-\\u9fa5)]{1,30}";
+
+    // 半角 字母 数字  及汉字
+    public static final String LABEL_NAME_PATTERN = "[(a-zA-Z0-9\\u4e00-\\u9fa5)]{1,10}";
+
+
+    /**
+     * 判断对象是否为空
+     * null				-> true
+     * ""				-> true
+     * " "				-> true
+     * "null"			-> true
+     * empty Collection	-> true
+     * empty Array		-> true
+     * others			-> false
+     *
+     * @param object
+     * @return
+     */
+    public static boolean isEmpty(Object object) {
+        if (object == null) {
+            return true;
+        } else if (object instanceof String) {
+            return ((String) object).trim().equals("") || ((String) object).trim().equals("null");
+        } else if (object instanceof Long) { //增加long参数的判断
+            return ((Long) object).longValue() == 0l;
+        } else if (object instanceof Collection<?> || object instanceof Map<?, ?>) {
+            return ((Collection<?>) object).isEmpty();
+        } else if (object.getClass().isArray()) {
+            return Array.getLength(object) == 0;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isNotEmpty(Object object) {
+        return !isEmpty(object);
+    }
+
+    public static boolean isArray(Object obj) {
+        return (obj != null && obj.getClass().isArray());
+    }
+
+    public static boolean isCheckedException(Throwable ex) {
+        return !(ex instanceof RuntimeException || ex instanceof Error);
+    }
+
+
+
+
+    /**
+     * 价格的正则表达式的验证
+     *
+     * @param str
+     * @return
+     */
+    public static boolean validPrice(String str) {
+        Pattern pattern = Pattern.compile("^(([1-9]{1}\\d{0,6})|([0]{1}))(\\.(\\d){0,2})?$"); // 判断小数点后一位的数字的正则表达式
+        //java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("^(([1-9]{1}\\d{0,6})|([0]{1}))(\\.(([1-9]{1}\\d{0,6})|([0]{1})){0,2})?$"); // 判断小数点后一位的数字的正则表达式
+        java.util.regex.Matcher match = pattern.matcher(str);
+        if (match.matches() == false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+
+
+    /**
+     * 验证 内容是否包含数据库不能识别的字符  特别是EMOJ表情
+     * 是否含有四字节的utf8
+     *
+     * @param s
+     * @return true含有    false不含有
+     */
+    public static boolean has4Butf8(String s) {
+        byte[] bs = s.getBytes(Charset.forName("utf8"));
+        int size = bs.length;
+        byte b;
+        for (int i = 0; i < size; i++) {
+            b = bs[i];
+            if ((b & 0xF0) == 0xF0 && (i + 3 < size)) {
+                if ((bs[i + 1] & 0x80) == 0x80 &&
+                        (bs[i + 2] & 0x80) == 0x80 &&
+                        (bs[i + 3] & 0x80) == 0x80) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * 将价格为分单位 转成  元为单位
+     * 1100-> 11
+     * 1110 -> 11.1
+     * 1111 -> 11.11
+     *
+     * @param priceFen
+     * @return
+     */
+    public static String formatProductPrice(Long priceFen) {
+        if (null != priceFen) {
+            Double price = Double.parseDouble(priceFen.toString());
+            DecimalFormat df = new DecimalFormat("0.##");
+            return df.format(price / 100);
+        }
+        return "";
+    }
+
+    public static  String newStr(Long id){
+        return StringUtils.leftPad(String.valueOf(id), 11, "0");
+    }
+
+    public static void main(String[] args){
+        System.out.println(newStr(0l));
+        System.out.println(newStr(110l));
+        System.out.println(newStr(1234567890l));
+        System.out.println(newStr(12345678901l));
+        System.out.println(newStr(123456789012l));
+    }
+}
