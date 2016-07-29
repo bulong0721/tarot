e1c9890d6e328f368d2adbeb8ca8e82e216177f7
martin <martin.xu@clever-m.com>
2016-07-29 09:18:25 +0800

    BootStrapFacatoryBean改为annotation发布，并改为单例。

--- common/tarot-core-service/src/main/java/com/myee/tarot/weixin/service/impl/BootStrapFacatoryBean.java
+++ common/tarot-core-service/src/main/java/com/myee/tarot/weixin/service/impl/BootStrapFacatoryBean.java
@@ -6,41 +6,40 @@ import com.myee.djinn.server.operations.OperationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-import javax.inject.Singleton;
 
 /**
  * Created by Ray.Fu on 2016/6/30.
  */
 @Service
-@Scope("singleton")
 public class BootStrapFacatoryBean extends AbstractFactoryBean<ServerBootstrap> {
     @Value("${djinn.port}")
-    private int djinnPort;
+    private int               djinnPort;
     @Autowired
-    private OperationsService operationsService;
+    private OperationsService optService;
+
+    private static ServerBootstrap instance;
 
     @Override
     public Class<?> getObjectType() {
-        return null;
+        return ServerBootstrap.class;
     }
 
     @Override
-    protected ServerBootstrap createInstance() throws Exception {
-        final NettyServerConfig nettyServerConfig = new NettyServerConfig();
-        nettyServerConfig.setListenPort(djinnPort);
-        ServerBootstrap bootstrap = new ServerBootstrap(nettyServerConfig);
-        bootstrap.initialize();
-        try {
-            bootstrap.export(operationsService, OperationsService.class);
-        } catch (Exception e) {
-            e.printStackTrace();
+    protected synchronized ServerBootstrap createInstance() throws Exception {
+        if (null == instance) {
+            final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+            nettyServerConfig.setListenPort(djinnPort);
+            instance = new ServerBootstrap(nettyServerConfig);
+            instance.initialize();
+            try {
+                instance.export(optService, OperationsService.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            instance.start();
+            System.out.println("here");
         }
-
-        bootstrap.start();
-        System.out.println("here");
-        return bootstrap;
+        return instance;
     }
 }
