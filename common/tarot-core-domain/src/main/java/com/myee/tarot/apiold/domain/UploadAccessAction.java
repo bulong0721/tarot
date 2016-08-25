768c591336e459efa7d617ec2c8630653516549e
chay.ni <chay.ni@clever-m.com>
2016-08-25 11:50:31 +0800

    新增点点笔点击事件名称表

---
+++ common/tarot-core-domain/src/main/java/com/myee/tarot/apiold/domain/UploadAccessAction.java
@@ -0,0 +1,70 @@
+package com.myee.tarot.apiold.domain;
+
+import com.myee.tarot.core.GenericEntity;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+
+@Entity
+@javax.persistence.Table(name = "ca_smp_upload_action")//点点笔点击行为，支持多级级联
+public class UploadAccessAction extends GenericEntity<Long, UploadAccessAction> {
+	@Id
+	private Long id;
+
+	@NotEmpty
+	@Column(name = "NAME")
+	private String name;
+
+	@Column(name = "LEVEL")
+	private Integer level;//点击行为级别，1级，2级等
+
+	@Column(name = "PARENT")
+	private Long parent;//如果是2级以上点击行为，则应该有关联的父级行为ID
+
+	@Column(name = "ACTIVE", columnDefinition = "INT",length = 1)
+	private int active;
+
+	public UploadAccessAction(){
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+
+	public Long getParent() {
+		return parent;
+	}
+
+	public void setParent(Long parent) {
+		this.parent = parent;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+}
