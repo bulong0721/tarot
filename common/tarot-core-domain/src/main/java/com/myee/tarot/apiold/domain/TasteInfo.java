13cc2ad3987fbfa310d16a4e63edf2fa20293fb7
chay.ni <chay.ni@clever-m.com>
2016-08-29 13:39:51 +0800

    移植点点笔获取口味api接口

---
+++ common/tarot-core-domain/src/main/java/com/myee/tarot/apiold/domain/TasteInfo.java
@@ -0,0 +1,55 @@
+package com.myee.tarot.apiold.domain;
+
+import com.myee.tarot.core.GenericEntity;
+
+import javax.persistence.*;
+
+/**
+ * Info: clever
+ * User: Gary.zhang@clever-m.com
+ * Date: 1/20/16
+ * Time: 14:17
+ * Version: 1.0
+ * History: <p>如果有修改过程，请记录</P>
+ */
+@Entity
+@javax.persistence.Table(name = "CA_TASTE")
+public class TasteInfo extends GenericEntity<Long, MenuInfo> {
+
+    @Id
+    @Column(name = "ID", unique = true, nullable = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)//AUTO：主键由程序控制。IDENTITY：主键由数据库自动生成（主要是自动增长型） ； SEQUENCE：根据底层数据库的序列来生成主键，条件是数据库支持序列（类似oracle）；TABLE：使用一个特定的数据库表格来保存主键。
+    private Long id;
+
+    @Column(name = "TASTE_ID",length = 15)
+    private String tasteId;     //口味编码
+
+    @Column(name = "NAME")
+    private String name;       //名称
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTasteId() {
+        return tasteId;
+    }
+
+    public void setTasteId(String tasteId) {
+        this.tasteId = tasteId;
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ common/tarot-core-service/src/main/java/com/myee/tarot/apiold/dao/TasteDao.java
@@ -0,0 +1,10 @@
+package com.myee.tarot.apiold.dao;
+
+import com.myee.tarot.apiold.domain.TasteInfo;
+import com.myee.tarot.core.dao.GenericEntityDao;
+
+/**
+ * Created by Chay on 2016/8/10.
+ */
+public interface TasteDao extends GenericEntityDao<Long, TasteInfo> {
+}
