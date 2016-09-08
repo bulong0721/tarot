eb7be08bd8e986d29f1f61453be8a50d65472446
Jelynn <tanglin330@163.com>
2016-09-08 19:59:27 +0800

    添加接口

--- common/tarot-core-service/src/main/generated-java/com/myee/tarot/djinn/service/impl/DataStoreServiceImpl.java
+++ common/tarot-core-service/src/main/generated-java/com/myee/tarot/djinn/service/impl/DataStoreServiceImpl.java
@@ -1,6 +1,7 @@
 package com.myee.tarot.djinn.service.impl;
 
-import com.myee.djinn.dto.UploadFileType;
+import com.myee.djinn.dto.DataUploadInfoDTO;
+import com.myee.djinn.dto.UploadResourceType;
 import com.myee.djinn.rpc.RemoteException;
 import com.myee.djinn.server.operations.DataStoreService;
 import com.myee.tarot.core.service.TransactionalAspectAware;
@@ -11,13 +12,19 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class DataStoreServiceImpl implements DataStoreService, TransactionalAspectAware {
-    @Override
-    public int receiveLog(long l, UploadFileType uploadFileType, String s) throws RemoteException {
-        return 0;
-    }
 
-    @Override
+	@Override
+	public int receiveLog(long orgId, UploadResourceType fileType, String logText) throws RemoteException {
+		return 0;
+	}
+
+	@Override
     public int receiveText(long l, String s) throws RemoteException {
         return 0;
     }
+
+	@Override
+	public boolean uploadData(DataUploadInfoDTO dataUploadInfoDTO) throws RemoteException {
+		return false;
+	}
 }
