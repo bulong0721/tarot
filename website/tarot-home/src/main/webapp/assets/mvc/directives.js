8cf6c18e3e77f14909bd381da10214cff10a3d2e
martin <martin.xu@clever-m.com>
2016-09-28 15:34:02 +0800

    ng-table滚动条

--- website/tarot-home/src/main/webapp/assets/mvc/directives.js
+++ website/tarot-home/src/main/webapp/assets/mvc/directives.js
@@ -299,6 +299,46 @@ function cVideo(){
     }
 }
 
+function iboxToolsFullScreen($timeout) {
+    return {
+        restrict: 'A',
+        scope: true,
+        templateUrl: 'views/common/ibox_tools_full_screen.html',
+        controller: function ($scope, $element) {
+            // Function for collapse ibox
+            $scope.showhide = function () {
+                var ibox = $element.closest('div.ibox');
+                var icon = $element.find('i:first');
+                var content = ibox.find('div.ibox-content');
+                content.slideToggle(200);
+                // Toggle icon from up to down
+                icon.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+                ibox.toggleClass('').toggleClass('border-bottom');
+                $timeout(function () {
+                    ibox.resize();
+                    ibox.find('[id^=map-]').resize();
+                }, 50);
+            };
+            // Function for close ibox
+            $scope.closebox = function () {
+                var ibox = $element.closest('div.ibox');
+                ibox.remove();
+            };
+            // Function for full screen
+            $scope.fullscreen = function () {
+                var ibox = $element.closest('div.ibox');
+                var button = $element.find('i.fa-expand');
+                $('body').toggleClass('fullscreen-ibox-mode');
+                button.toggleClass('fa-expand').toggleClass('fa-compress');
+                ibox.toggleClass('fullscreen');
+                setTimeout(function() {
+                    $(window).trigger('resize');
+                }, 100);
+            }
+        }
+    };
+}
+
 /**
  *
  * Pass all functions into module
@@ -315,3 +355,4 @@ angular
     .directive("alerts", alerts)
     .directive("cDatepicker", cDatepicker)
     .directive("cVideo", cVideo)
+    .directive('iboxToolsFullScreen', iboxToolsFullScreen);
