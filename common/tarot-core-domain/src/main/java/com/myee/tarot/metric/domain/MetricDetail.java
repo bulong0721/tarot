4d927b2e60ed4e492c4dda36075650bde4350b9d
chay.ni <chay.ni@clever-m.com>
2016-11-29 10:15:42 +0800

    拼写错误

--- common/tarot-core-domain/src/main/java/com/myee/tarot/metric/domain/MetricDetail.java
+++ common/tarot-core-domain/src/main/java/com/myee/tarot/metric/domain/MetricDetail.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
  */
 
 @Entity
-@Table(name = "M_MATRIC_DETAIL")
+@Table(name = "M_METRIC_DETAIL")
 @DynamicUpdate //hibernate部分更新
 public class MetricDetail extends GenericEntity<Long, MetricDetail> {
 
