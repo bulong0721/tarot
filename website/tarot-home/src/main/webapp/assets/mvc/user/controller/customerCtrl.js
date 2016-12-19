d6e264c494391da3f3c300971560bb09db8d8cab
Ray.Fu <ray.fu@clever-m.com>
2016-12-19 18:26:37 +0800

    普通用户和普通用户角色传参

--- website/tarot-home/src/main/webapp/assets/mvc/user/controller/customerCtrl.js
+++ website/tarot-home/src/main/webapp/assets/mvc/user/controller/customerCtrl.js
@@ -305,7 +305,7 @@ function customerMgrCtrl($scope, cTables, cResource,$filter,$q,cfromly,NgTablePa
             return deferred.promise;
         } else {//第一次需要从后台读取列表，且只返回前10个数据
             return cResource.get('./listPermission/list',{
-                isFriendly: true
+                isFriendly: false
             }).then(function(data){
                 $scope.treeData = data.rows;
                 $scope.initalBindPermissionList = data.rows;
